576ffb524452ec212f6649b7a3d60fd2d91e68e6
Diego Eiras <eiras.lucio@gmail.com>
2020-08-04 22:40:49 -0300

    Dia 2: API RESTFUL com Node.js

---
+++ server/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+
+import ClassesController from './controllers/Classes'
+import ConnectionsController from './controllers/Connections'
+
+const routes = Router()
+
+routes.get('/classes', ClassesController.index)
+routes.post('/classes', ClassesController.create)
+
+routes.get('/connections', ConnectionsController.index)
+routes.post('/connections', ConnectionsController.create)
+
+export default routes
