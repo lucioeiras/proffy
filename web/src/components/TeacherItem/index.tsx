3eb194519a6a4f66ef520f11e4a1eefa5b218164
Diego Eiras <eiras.lucio@gmail.com>
2020-08-09 18:19:10 -0300

    Dia 3: Continuação do site com ReactJS

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,37 +2,60 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number
+  name: string
+  avatar: string
+  bio: string
+  whatsapp: string
+  subject: string
+  cost: number
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img 
-          src="https://avatars0.githubusercontent.com/u/67290471?s=460&u=3a40833b6b1e19b81017cb915f8af816f1e51ea7&v=4" 
-          alt="Diego Eiras"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Diego Eiras</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /> <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 mil pessoas já passaram pelas minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 1000,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`} 
+          type="button"
+          onClick={createNewConnection}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
